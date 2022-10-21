31e3877e1427a9ce7637386e05dbcfb7bc7f88d2
mariamksalama <mariam_salama97@hotmail.com>
2022-10-21 19:45:24 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Video-app';
+
+  ngOninit(){
+    console.log("idek")
+  }
+}
